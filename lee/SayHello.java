20fa584f3ba21aa8f6b820c5d69665833665fd6b
liwenda <you@example.com>
2019-09-11 16:40:55 +0800

    class

---
+++ lee/SayHello.java
@@ -0,0 +1,20 @@
+public class SayHello {
+       public static void main(String[]  ages) {
+              System.out.println("Below is the function of sayhello");
+               sayhello(sum());
+               HelloWold hellowold = new Hellowold() ;
+                hellowold.sayhello(hellowold.sum());
+
+       }
+       public static void sayhello(int sum){
+              System.out.println("say hello!"+sum);
+       }
+       public static int sum(){
+              int i;
+              for (i=0;i<=100;i++){
+                     sum=sum+i; 
+              }
+              return sum;
+      }
+
+ }
