d578c323666498d45237bbc05116af3a905a5b4b
liwenda <you@example.com>
2019-09-11 16:43:15 +0800

    frist

--- li/SayHello.java
+++ li/SayHello.java
@@ -1,5 +1,5 @@
 public class SayHello {
-       public static void main(String[]  ages) {
+       public static void main(String[]  args) {
               System.out.println("Below is the function of sayhello");
                sayhello(sum());
                HelloWold hellowold = new Hellowold() ;
