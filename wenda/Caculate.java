31bf014dbf3d36959b9737fd8fa69a44f927395c
liwenda <you@example.com>
2019-09-16 21:43:35 +0800

    000

---
+++ wenda/Caculate.java
@@ -0,0 +1,46 @@
+public class Caculate {
+    public static void main(StrinG[] args){
+		System.out.println ("34 + 30 = "+34+30){
+                                                int count = 10;
+		
+                                                count *= count + 2;
+		
+                                                count /=100;
+		
+                                                count ++;
+        
+                                 System.out.printf("%d",count);
+
+			int res=0 ;
+			int n1 = 12;
+			int n2 = 24; 
+			int n3 = 16; 
+			int n4 = 3;
+			double d ;
+			double v1 = 10.0;
+			double v2 =3.1416;
+			
+			res = n1/n4;
+			System.out.printf("res = %d ",res);
+			d = n1/n4;
+			System.out.printf("d = %f",d);
+			res = n3/n4;
+			System.out.printf("res = %d",res);
+			d = n3/n4;
+			System.out.printf("d = %f",d);
+			d = v1/n4;
+			System.out.printf("d = %f",d);
+			d = v1/n2;
+			System.out.printf("d = %f",d);
+			res = n1/n2;
+			System.out.printf("res = %d",res);
+			d = (double)n1/n2;
+			System.out.printf("d = %f",d);
+			d = n1/(double)n2;
+			System.out.printf("d = %f",d);
+			d = (double)(n1/n2);
+			System.out.printlf("d = %f",d);
+			
+		} 
+	 
+}
