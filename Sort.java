115df7fa28201348d3b8d7023a3cec366980d3cd
liwenda <you@example.com>
2019-10-08 18:09:35 +0800

    window

---
+++ Sort.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class Sort{
+	public static void main(String[] args){
+		int a[] =new int[]{9,5,2,7,22,12,3,8};
+		Arrays.sort(a);
+		for(int b:a){
+			System.out.println(b+",");
+	    }
+    }
+}
