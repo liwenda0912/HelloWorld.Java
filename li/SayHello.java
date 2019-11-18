ba15360b111429ed52ea9736e5b8fd0c63d716c0
liwenda <you@example.com>
2019-11-18 18:21:59 +0800

    000

--- li/SayHello.java
+++ li/SayHello.java
@@ -1,18 +1,19 @@
 public class SayHello {
        public static void main(String[]  args) {
+           int sum;
               System.out.println("Below is the function of sayhello");
                sayhello(sum());
-               HelloWold hellowold = new Hellowold() ;
-               hellowold.sayhello(hellowold.sum());
+              SayHello helloworld = new SayHello() ;
+               helloworld.sayhello(helloworld.sum());
 
        }
        public static void sayhello(int sum){
               System.out.println("say hello!"+sum);
        }
        public static int sum(){
-              int i;
-              for (i=0;i<=100;i++){
-                     sum=sum+i; 
+              int i,sum=0;
+           for (i=0; i<=100; i++){
+                     sum=sum+i;
               }
               return sum;
       }
